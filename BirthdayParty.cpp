dd91afdac6654d379bd6936509c72c07a53ffb3e
srimani-programmer <srimani.scholar@gmail.com>
2018-10-04 19:46:50 +0530

    Solution

---
+++ BirthdayParty.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int members,choclates;
+        cin>>members>>choclates;
+
+        if(choclates % members == 0)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+        
+    }
+
+    return 0;
+}
