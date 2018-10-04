6e670a5cd717938a507758e620a5809772034389
srimani-programmer <srimani.scholar@gmail.com>
2018-10-04 06:46:44 +0530

    Solution

---
+++ Ladderophilia.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int val;
+    cin>>val;
+
+    while(val--){
+        for(int i=0;i<2;i++)
+            cout<<"*   *"<<endl;
+        
+        cout<<"*****"<<endl;
+    }
+        for(int i=0;i<2;i++)
+            cout<<"*   *"<<endl;
+    return 0;
+}
